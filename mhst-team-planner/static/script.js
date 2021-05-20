93509b0cb66a24899d72eed90deb695ac855ffeb
gh4ever <gh4ever101@gmail.com>
2021-05-20 13:45:36 -0400

    Drag and drop working

--- mhst-team-planner/static/script.js
+++ mhst-team-planner/static/script.js
@@ -1,31 +1,93 @@
-function addToTeam($this) {
-    // check for a free team slot
-    var $freeSlot = $("ul.team .empty").first();
-    if ($freeSlot.length <= 0) {
-	return;
+function drag(e, dragged){
+    e.dataTransfer.setData("text/html", dragged.outerHTML);
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    var $dropped = $(e.dataTransfer.getData("text/html"));
+    if ($dropped.hasClass("delement")) {
+	changeElement($(e.currentTarget), $dropped)
+    } else {
+	addToTeam($(e.currentTarget), $dropped)
     }
-    $freeSlot.removeClass("empty");
+}
+
+function filterMonsties() {
+    var $filters = $(".filter");
+    var $monsties = $(".monsties").children();
+    for (var i = 0; i < $monsties.length; i++) {
+	$monstie = $monsties.eq(i);
+	$monstie.removeClass("hidden");
+	for (var j = 0; j < $filters.length; j++) {
+	    var keys = $filters.eq(j).val();
+	    var hasKey = false;
+	    for (var k = 0; k < keys.length; k++) {
+		if ($monstie.hasClass(keys[k])) {
+		    hasKey = true;
+		    break;
+		}
+	    }
+	    if (hasKey == false) {
+		$monstie.addClass("hidden");
+	    }
+	}
+    }
+}
+
+var hashes = ["", "",
+	      "", "",
+	      "", "",
+	      "", "",
+	      "", "",
+	      "", ""]
+
+function changeElement($target, $dropped) {
+    if ($target.hasClass("empty")) {
+	return;
+    } else {
+	var metadata = $dropped.attr("class").split(' ');
+	var newElement;
+	for (var i = 0; i < metadata.length; i++) {
+	    if (metadata[i] === "delement") {
+		continue;
+	    } else {
+		newElement = metadata[i];
+		$target.children("figure.tfig").children("div.elements").attr("class", "elements "+newElement);
+	    }
+	}
+	hashes[2*$target.index()+1] = newElement;
+	window.location.hash = hashes.toString();
+	$(".url")[0].value = window.location.href;
+    }
+}
+
+function addToTeam($target, $dropped) {
+    // toggle the emptiness of the target
+    $target.attr("class", "");
 
     // get the various elements
-    var $figure = $freeSlot.children("figure.tfig");
+    var metadata = $dropped.attr("class").split(' ');
+    var $icon =$dropped.children("div");
+    var $figure = $target.children("figure.tfig");
     var $monstie = $figure.children("a.icons");
     var $element = $figure.children("div.elements")
-    var $info = $freeSlot.children("div.info");
+    var $info = $target.children("div.info");
     var $name = $info.children("span.name");
     var $type = $info.children("span.type");
 
     // set the attributes of the monstie
-    $monstie.attr("class", $this.attr("class"));
+    $monstie.attr("class", $icon.attr("class"));
     $monstie.attr("href", "#!");
     $monstie.attr("onclick", "return removeFromTeam($(this));");
-    $name.html($this.attr("title"));
-
-    // get the metadata for the monstie
-    var metadata = $this.parent().attr("class").split(' ');
+    $name.html($icon.attr("title"));
 
     // parse through the metadata
-    for (var k = 0; k < metadata.length; k++) {
-	switch(metadata[k]) {
+    var newElement = "";
+    for (var i = 0; i < metadata.length; i++) {
+	switch(metadata[i]) {
 	case "power":
 	    $type.html("Power");
 	    break;
@@ -35,22 +97,31 @@ function addToTeam($this) {
 	case "technical":
 	    $type.html("Technical");
 	    break;
+	case "nelement":
+	    $element.attr("class", "elements nelement");
+	    break;
 	case "dragon":
 	case "fire":
 	case "ice":
 	case "thunder":
 	case "water":
-	    $element.attr("class", "elements "+metadata[k]);
+	    newElement = metadata[i];
+	    $element.attr("class", "elements "+newElement);
 	    break;
 	default:
 	    break;
 	}
     }
+    hashes[2*$target.index()] = $icon.attr("title").replace(/ /g, "_");
+    hashes[2*$target.index()+1] = newElement;
+    window.location.hash = hashes.toString();
+    $(".url")[0].value = window.location.href;
 }
 
 function removeFromTeam($this) {
     // find the relevant elements
     var $figure = $this.parent();
+    var $li = $figure.parent();
     var $element = $figure.children("div.elements")
     var $li = $figure.parent();
     var $info = $li.children("div.info");
@@ -58,20 +129,41 @@ function removeFromTeam($this) {
     var $type = $info.children("span.type");
 
     // clear the relevant attributes
-    $li.addClass("empty");
     $this.attr("class", "icons unknown_monster");
     $this.removeAttr("href");
     $this.removeAttr("onclick");
-    $element.attr("class", "nelement");
+    $element.attr("class", "elements nelement");
     $name.html("???");
     $type.html("???");
+    $li.attr("class", "empty");
+    hashes[2*$li.index()] = "";
+    hashes[2*$li.index()+1] = "";
+    window.location.hash = hashes.toString();
+    $(".url")[0].value = window.location.href;
 
     // return false to not reload the page
     return false;
 }
 
 $(document).ready(function() {
-    $("ol.monsties .icons").click(function(e) {
-	addToTeam($(this));
-    });
+    // load the filters
+    $(".filter").multiselect();
+
+    // load the team if there was a hash
+    if (window.location.hash !== "") {
+	var original_hashes = window.location.hash.split('#')[1].split(',');
+	for (var i = 0; i < original_hashes.length; i++) {
+	    if (original_hashes[i] === "") {
+		continue;
+	    }
+	    var $target = $(".teamlist").children().eq(Math.floor(i/2));
+	    if (i%2 == 0) {
+		addToTeam($target, $("div[title='"+original_hashes[i].replace(/_/g, ' ')+"']").parent());
+	    } else {
+		console.log($(".delement."+original_hashes[i]));
+		changeElement($target, $(".delement."+original_hashes[i]));
+	    }
+	}
+    }
+    $(".url")[0].value = window.location.href;
 });
